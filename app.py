6e6ef27dbc78a1b2237b5b4ec910da20bc76ec33
wtfychxx <fadhliyulyanto@gmail.com>
2025-01-15 22:24:39 +0700

    add upload button

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ with open('utils/label_encoder.pkl', 'rb') as file:
 st.title("Energy Consumption Prediction")
 
 relativeCompactness = st.text_input("Relative Compactness")
-surfaceArea = st.text_input("Surface Area")
-roofArea = st.text_input("Roof Area")
+surfaceArea = st.text_input("Surface Area(M)")
+roofArea = st.text_input("Roof Area(M)")
 overallHeight = st.text_input("Overall Height")
+
+uploaded_file = st.file_uploader("Untuk prediksi masal, upload file", type=["xlsx", "xls"])
 
 data = {
     "X1": relativeCompactness,
@@ -37,15 +39,22 @@ def transform_data(data, encoders):
             transformed_data[key] = value
     return pd.DataFrame([transformed_data])
 
+def batch_predictions(data, model):
+    predictions = model.predict(data)
+    return predictions
+
 # Tombol Prediksi
 if st.button("Predict"):
     try:
-        input_data = transform_data(data, encoders)
+        if uploaded_file:
+            result = batch_predictions(transform_data, model)
+        else:
+            input_data = transform_data(data, encoders)
 
-        result = make_prediction(input_data, model)
-        
-        st.write("Hasil Prediksi: \n")
-        st.write(f"Heating Load: {result[0]} \n")
-        st.write(f"Cooling Load: {result[1]} \n")
+            result = make_prediction(input_data, model)
+            
+            st.write("Hasil Prediksi: \n")
+            st.write(f"Heating Load: {result[0]} \n")
+            st.write(f"Cooling Load: {result[1]} \n")
     except Exception as e:
         st.error(f"Terjadi Kesalahan: {e}")
